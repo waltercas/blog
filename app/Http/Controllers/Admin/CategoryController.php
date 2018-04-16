8753355b05dcd0433f55fb894c72f6cdb6aafc33
Walter Castillo <waltercastillo@live.com.ar>
2018-04-16 01:18:35 -0300

    modulo caterorias completo

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,85 +2,78 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+
 use App\Http\Controllers\Controller;
+
+use App\Category;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        $category = Category::all();
+        $categories = Category::orderBy('id', 'DESC')->paginate();
+        return view('admin.categories.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.categories.create');  
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+ /**   public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name'=>'required',
+            'slug'=>'required|unique:categories,slug',
+        ]);
+        $category = Category::create($request->all());
+
+        return redirect()->route('categories.edit', $category->id)->with('info', 'Etiqueta creada con éxito');
+    }*/        
+
+    public function store(CategoryStoreRequest $request)
+    {
+         $category = Category::create($request->all());
+
+        return redirect()->route('categories.edit', $category->id)->with('info', 'Etiqueta creada con éxito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
+    public function show($id)
     {
-        //
+        $category = Category::find($id);
+
+        return view('admin.categories.show', compact('category'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
+    public function edit($id)
     {
-        //
+        $category = Category::find($id);
+
+        return view('admin.categories.edit', compact('category'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
+    public function update(CategoryUpdateRequest $request, $id)
     {
-        //
+        $category = Category::find($id);
+ 
+        $category->fill($request->all())->save();
+
+        return redirect()->route('categories.edit', $category->id)
+            ->with('info', 'Categoria actualizada con exito');
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
+    public function destroy($id)
     {
-        //
+        $category = Category::find($id)->delete();
+        return back()->with('info', 'Eliminado correctamente');
     }
 }
